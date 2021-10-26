0e377e05efecf34f2b43eac8bc5486efe6d27c28
JonelSV <svjonel@gmail.com>
2021-10-25 21:17:47 -0400

    first commit: created first package

---
+++ finalProjectBend2/src/main/java/com/finalProjectBend/FinalProjectBend2Application.java
@@ -0,0 +1,13 @@
+package com.finalProjectBend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectBend2Application.class, args);
+	}
+
+}
---
+++ finalProjectBend2/src/test/java/com/finalProjectBend/FinalProjectBend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProjectBend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectBend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
